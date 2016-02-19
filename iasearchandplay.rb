775618e5435e6669faea32cd4c949b2aeda1e649
Wouter Scheivink <wjs12@hampshire.edu>
2016-02-19 13:20:45 -0500

    sucessfully refactored so no need to use bad python cli

--- iasearchandplay.rb
+++ iasearchandplay.rb
@@ -1,32 +1,43 @@
 require 'rubygems'
 require 'trollop'
+require 'httparty'
 require 'pry'
-require 'highline/import'
+require 'open-uri'
 
-opts = Trollop::options do
-  opt :title, "The title", :type  => :string
+params = Trollop::options do
+  opt :query, "The search query", :type  => :string
+  opt :rows, "", :type  => :int, :default => 15
+  opt :file_dir, "", :type  => :string, :default => '/tmp/randarch.ogg'
+end
+def main params
+  files = get_list_of_files params
+  chosen_file = files.sample["identifier"]
+  puts "Selected #{chosen_file}"
+  download_url = get_download_url(files.sample["identifier"])
+  download_file(params[:file_dir], download_url)
+  puts "Downloaded"
+  play = `mplayer #{params.file_dir}`
 end
 
-def main opts
-  command = "ia search 'title:#{opts[:title]} mediatype:audio' --itemlist"
-  results = `#{command}`
-  split_results = results.split("\n")
-  puts split_results
-  begin 
-    downloaded_result = get_download_from split_results
-    puts "playing #{downloaded_result}"
-    play = `mplayer ./#{downloaded_result}/*.ogg`
-  ensure
-    FileUtils.remove_dir("./#{downloaded_result}", force = true)
-  end until ask "Stop (y/n)" == "n"
+def get_list_of_files params
+  url = "https://archive.org/advancedsearch.php?q=#{params.query} AND mediatype:audio&fl[]=identifier,title,mediatype&rows=#{params.rows}&output=json" 
+  response = HTTParty.get(url)
+  p_response = response.parsed_response
+  return p_response["response"]["docs"]
 end
 
-def get_download_from results
-  begin
-    chosen_result = results.sample
-    download = `ia download #{chosen_result} --glob='*.ogg'`
-  end until !download.include? "no matching files found"
-  return chosen_result
+def get_download_url identifier
+  url = "https://archive.org/metadata/#{identifier}"
+  response = HTTParty.get(url)
+  p_response = response.parsed_response
+  names = p_response["files"].select { |e| e["name"].include? ".ogg" }
+  return "http://archive.org/download/#{identifier}/#{names.sample["name"]}"
 end
 
-main opts
+def download_file file_dir, url
+  open(file_dir, 'wb') do |file|
+    file << open(url).read
+  end
+end
+
+main params
