e880f8515a81eb7a1812bed432f2eff25e0f6f10
Wouter Scheivink <wjs12@hampshire.edu>
2016-02-19 15:17:09 -0500

    added begining of rspec tests

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'trollop'
+gem 'highline'
+gem 'pry'
+gem 'httparty'
+gem 'rspec'
---
+++ spec/archive_autoplay_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Archive Autoplay" do
+  describe "#get_list_of_files" do
+    binding.pry
+   get_list_of_files().should == ""
+  end
+end
